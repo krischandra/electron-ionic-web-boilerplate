b9ff0e22801b5f97cca67954f303d44836227b7f
krishna.chandra <krishna.chandra4@t-mobile.com>
2018-11-04 20:32:37 -0800

    initial scaffolding

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+# Cordova integration.
+
+1) npm install -g cordova
+2) cordova create mobile com.krishna.app "mobile"
+3) cd ElectronIonicWebBoilerPlate
+4) cordova platform add browser || cordova platform add ios
+5) cordova run browser || cordova run --target=chrome --port=5000 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,55 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ComposeMessageComponent} from './compose-message/compose-message.component';
+import {AuthGuard} from './auth/auth.guard';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AuthModule} from './auth/auth.module';
+import {HeroesModule} from './heroes/heroes.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+const appRoutes: Routes = [
+  {
+    path: 'compose',
+    component: ComposeMessageComponent,
+    outlet: 'popup'
+  },
+  {
+    path: 'admin',
+    loadChildren: './admin/admin.module#AdminModule',
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'crisis-center',
+    loadChildren: './crisis-center/crisis-center.module#CrisisCenterModule',
+    data: { preload: true }
+  },
+  { path: '',   redirectTo: '/superheroes', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComposeMessageComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HeroesModule,
+    AuthModule,
+    AppRoutingModule,
   ],
   providers: [],
+  exports: [
+    RouterModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
